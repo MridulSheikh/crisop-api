b85fc5a77853e9c226b9da04b67a2401e3553de5
MridulSheikh <mridul.sheikh.90@gmail.com>
2025-09-11 12:51:47 +0600

    add user role

--- src/app/modules/stock/stock.route.ts
+++ src/app/modules/stock/stock.route.ts
@@ -3,19 +3,20 @@ import auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
 import { createStockValidationSchema, updateStockValidationSchema } from "./stock.validation";
 import { createStockeIntoDbController, getAllStockFromDBController, getSingleStockFromDBController, softDeleteController, updateStockController } from "./stock.contorller";
+import { UserRole } from "../user/user.interface";
 
 
 const Router = express();
 
 Router.route('/')
-.post(auth("admin", "manager"), validateRequest(createStockValidationSchema), createStockeIntoDbController)
-.get(auth('admin', "manager"), getAllStockFromDBController)
+.post(auth(UserRole.admin, UserRole.manager, UserRole.super), validateRequest(createStockValidationSchema), createStockeIntoDbController)
+.get(auth(UserRole.admin, UserRole.manager, UserRole.super), getAllStockFromDBController)
 
 
 Router.route("/:id")
-.get(auth("admin", "manager"), getSingleStockFromDBController)
-.delete(auth("admin", "manager"), softDeleteController)
-.patch(auth("admin", "manager"),validateRequest(updateStockValidationSchema) , updateStockController)
+.get(auth(UserRole.admin, UserRole.manager, UserRole.super), getSingleStockFromDBController)
+.delete(auth(UserRole.admin, UserRole.manager, UserRole.super), softDeleteController)
+.patch(auth(UserRole.admin, UserRole.manager, UserRole.super),validateRequest(updateStockValidationSchema) , updateStockController)
 
 
 export default Router;
--- src/app/modules/warehouse/warehouse.route.ts
+++ src/app/modules/warehouse/warehouse.route.ts
@@ -4,16 +4,17 @@ import validateRequest from "../../middlewares/validateRequest";
 import { createWarehouseValidationSchema } from "./warehouse.validation";
 import { createWarehouseIntoDbController, deleteWarehouseController, getAllWarehouseFromDbController, getSingleWareHouseByIdFromDBController, updateWarehouseIntoDbController } from "./warehouse.controller";
 import { UpdateUserValidatonSchema } from "../user/user.validation";
+import { UserRole } from "../user/user.interface";
 
 const Router = express();
 
 
 Router.route("/")
-.post(auth("admin", "manager"), validateRequest(createWarehouseValidationSchema), createWarehouseIntoDbController)
-.get(auth("admin", "manager"), getAllWarehouseFromDbController)
+.post(auth(UserRole.admin, UserRole.manager, UserRole.super), validateRequest(createWarehouseValidationSchema), createWarehouseIntoDbController)
+.get(auth(UserRole.admin, UserRole.manager, UserRole.super), getAllWarehouseFromDbController)
 
 Router.route("/:id")
-.delete(auth("admin", "manager"), deleteWarehouseController)
+.delete(auth(UserRole.admin, UserRole.manager, UserRole.super), deleteWarehouseController)
 .get(auth("admin", "manager"), getSingleWareHouseByIdFromDBController)
-.patch(auth("admin", "manager"), validateRequest(UpdateUserValidatonSchema), updateWarehouseIntoDbController)
+.patch(auth(UserRole.admin, UserRole.manager, UserRole.super), validateRequest(UpdateUserValidatonSchema), updateWarehouseIntoDbController)
 export default Router;

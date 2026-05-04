ee04cf7ce1b0bd32bfcd6eaf00315f42cd9ecd15
MridulSheikh <mridul.sheikh.90@gmail.com>
2026-05-04 11:52:45 +0600

    fix cors policy

--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,7 @@ const client_url =
 // Middleware: JSON and cookie parser
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+
 // Enable CORS with credentials
 app.use(
   cors({
